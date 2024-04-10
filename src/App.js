b1fe4af15bc77f492ff16bee2a5c71294114ff67
SC00bidu <you@example.com>
2024-04-11 00:51:59 +0530

    filter,form for booking added and login removed

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./pages/Login";
 import Tickets from "./pages/Tickets";
+import Details from "./pages/Details";
 import NotFound from "./pages/NotFound";
 
 function App() {
   const router = createBrowserRouter([
-    { path: "/", element: <LoginPage /> },
-    { path: "/tickets", element: <Tickets /> },
+    // { path: "/", element: <LoginPage /> },
+    { path: "/", element: <Tickets /> },
+    { path: "/details/:id", element: <Details /> },
     { path: "/*", element: <NotFound /> },
   ]);
   return (
-    <div className="">
+    <div className="mx-10">
       <RouterProvider router={router} />
     </div>
   );
--- src/pages/Tickets.js
+++ src/pages/Tickets.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CreateTicket from "../components/CreateTicket";
+import UserForm from "../components/UserForm";
 import Ticketcard from "../components/Ticketcard";
 
 const TicketsPage = () => {
   const apiUrl = process.env.REACT_APP_BASE_URL;
   const [allTickets, setAllTickets] = useState([]);
-  const [showCreateModal, setShowCreateModal] = useState(false);
+  const [showTickets, setShowTickets] = useState([]);
+  let filterArray = ["All", 800, 900, 1000];
+  const [filter, setFilter] = useState("All");
   const [changes, setChanges] = useState(false);
 
   useEffect(() => {
@@ -22,27 +25,46 @@ const TicketsPage = () => {
     getTickets();
   }, [changes]);
 
+  useEffect(() => {
+    if (filter == 800) {
+      setShowTickets(allTickets.filter((item) => item.price == 800));
+    } else if (filter == 900) {
+      setShowTickets(allTickets.filter((item) => item.price == 900));
+    } else if (filter == 1000) {
+      setShowTickets(allTickets.filter((item) => item.price == 1000));
+    } else setShowTickets(allTickets);
+  }, [allTickets, filter]);
+
   return (
     <div>
-      <h2 className="text-2xl mb-4 font-bold">Tickets Page</h2>
-      <button
-        className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300"
-        onClick={() => setShowCreateModal(true)}
-      >
-        Create Ticket
-      </button>
+      <h2 className=" mt-5 text-3xl text-center mb-4 font-bold">Tickets</h2>
+
+      <div className="flex flex-col">
+        <h1>Filters by price:</h1>
+        <div className="flex gap-2">
+          {filterArray.map((item, ind) => (
+            <h1
+              key={ind}
+              onClick={() => setFilter(item)}
+              className={`border-2 px-2 rounded-full border-red-200 hover:border-red-400 ${
+                filter == item ? `bg-red-400 text-white border-red-400` : ``
+              }`}
+            >
+              {item}
+            </h1>
+          ))}
+        </div>
+      </div>
       <div className="flex flex-wrap gap-6 mx-8 mt-8">
-        {allTickets.map((item) => (
-          <Ticketcard item={item} changes={changes} setChanges={setChanges} />
+        {showTickets.map((item, ind) => (
+          <Ticketcard
+            key={ind}
+            item={item}
+            changes={changes}
+            setChanges={setChanges}
+          />
         ))}
       </div>
-      {showCreateModal && (
-        <CreateTicket
-          setShowCreateModal={setShowCreateModal}
-          changes={changes}
-          setChanges={setChanges}
-        />
-      )}
     </div>
   );
 };
